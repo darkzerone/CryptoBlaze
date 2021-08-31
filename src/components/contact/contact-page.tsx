11d9725a9da276824a2089fc611bc9915129b54c
Steven <steven@bendingtherules.nl>
2021-08-31 15:56:12 +0200

    removed warning

--- src/components/contact/contact-page.tsx
+++ src/components/contact/contact-page.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement } from "react";
 import classes from "./contact-page.module.scss";
 export const ContactPage = (): ReactElement => {
 
